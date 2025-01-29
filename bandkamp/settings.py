b6ddd2edf26e8c9d6a231e93f0492f94bb002fa1
Gabriel Zarpellon <gabriel_zv@hotmail.com>
2025-01-29 14:20:32 -0300

    feat: install and config drf-spectacular and create documentation routes

--- bandkamp/settings.py
+++ bandkamp/settings.py
@@ -44,9 +44,7 @@ DJANGO_APPS = [
     "django.contrib.staticfiles",
 ]
 
-THIRD_PARTY_APPS = [
-    "rest_framework",
-]
+THIRD_PARTY_APPS = ["rest_framework", "drf_spectacular"]
 
 MY_APPS = [
     "users",
@@ -132,6 +130,7 @@ SIMPLE_JWT = {
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 2,
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
 
 # Internationalization
@@ -157,3 +156,10 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "users.User"
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Bandkamp API",
+    "DESCRIPTION": "API de gerenciamento de usuários, álbuns e músicas",
+    "VERSION": "1.0.0",
+    "SERVE_INCLUDE_SCHEMA": False,
+}
